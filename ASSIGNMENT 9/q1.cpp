07ff26406c16b85b69c5f2ec8d23bcc5379634e0
amalik2006 <amalik_be24@thapar.edu>
2026-05-08 23:08:46 +0530

    Update q1.cpp

---
+++ ASSIGNMENT 9/q1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+    ofstream fout("NUM.TXT");
+
+    for(int i = 1; i <= 200; i++)
+        fout << i << " ";
+
+    fout.close();
+
+    return 0;
+}
